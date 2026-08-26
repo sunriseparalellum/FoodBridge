30f31c08e22448b07f6bbdde16b9e12f41db36db
ayazhan <ayazhan.karatayeva2005@gmail.com>
2026-08-27 01:18:54 +0500

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -3,4 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py createsuperuser --noinput || true
 python manage.py seed_production
